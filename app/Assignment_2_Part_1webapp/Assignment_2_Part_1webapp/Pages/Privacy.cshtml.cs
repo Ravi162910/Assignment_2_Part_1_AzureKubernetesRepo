d9fa1c583e05c86abd82d2e16fd2bc5f7f9a9d01
RaviPratap <ravip357090@gmail.com>
2026-09-24 23:20:14 +1200

    Initial commit

---
+++ app/Assignment_2_Part_1webapp/Assignment_2_Part_1webapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment_2_Part_1webapp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
